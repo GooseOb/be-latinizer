7f80c67be82f8a77d5b48febe4ef0c1eb9fd87e6
GooseOb <gooose.ob@gmail.com>
2024-11-02 04:42:03 +0100

    update taraskevizer

--- transform-content.ts
+++ transform-content.ts
@@ -1,15 +1,12 @@
-import { dicts, tarask, pipelines, TaraskConfig } from "taraskevizer";
+import { dicts, pipelines, TaraskConfig } from "taraskevizer";
 
 const cfg = new TaraskConfig({
-  general: { abc: dicts.alphabets.latinJi },
+  abc: dicts.alphabets.latinJi,
 });
 
 export const transformContent = (content: string) => {
-  return tarask(
-    content.replace(/\\n/g, " \\n ").replace(/ і(?=зь? )/g, " i"),
-    pipelines.plainText,
-    cfg,
-  )
+  return pipelines
+    .tarask(content.replace(/\\n/g, " \\n ").replace(/ і(?=зь? )/g, " i"), cfg)
     .replace(/(?:^| )\\n(?: |$)/g, "\\n")
     .replace(/(?<=[AOUEYIaoueyi]\s*\\n\s*)(?=i)/gu, "j")
     .replace(/(?<=[AOUEYIaoueyi]\s*\\n\s*)I(?=\p{Ll})/gu, "Ji")
